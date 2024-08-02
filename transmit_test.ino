59a4c0110a592fa5101a90342619a6a6b3e92903
taleb-mac <61245162+taleb-mac@users.noreply.github.com>
2024-08-02 18:17:25 +0300

    added functionality to receive packets to transmit through UART

--- transmit_test.ino
+++ transmit_test.ino
@@ -1,17 +1,18 @@
 #include <SPI.h>
 
 // Data Pins
-#define RECIEVE_PIN 1
-#define TRANSMIT_PIN 3
+#define RECIEVE_PIN 16
+#define TRANSMIT_PIN 17
 #define CLK_OUT_PIN 21
-
 // Setup Pins
 #define CE_PIN 5
 #define SREAD_PIN 19
 #define SDATA_PIN 23
 #define SCLK_PIN 18
 #define SLE_PIN 32
-
+// Flatsat tx/rx
+#define FS_TX_PIN 1
+#define FS_RX_PIN 3
 
 const uint32_t REG0 = 0x11616A20;
 const uint32_t REG1 = 0x00475011;
@@ -19,12 +20,22 @@ const uint32_t REG2 = 0x003DD082;
 const uint32_t REG3 = 0x2B149923; 
 const uint32_t REG15 = 0x000E000F;
 
+#define MAX_PACKET_SIZE 256 // Define a max size for telemetry packets
+#define BUFFER_SIZE 10      // Define the number of packets to store
+
+uint8_t telemetryBuffer[BUFFER_SIZE][MAX_PACKET_SIZE]; // Buffer to store telemetry packets
+uint8_t packetLengths[BUFFER_SIZE]; // Array to store the lengths of the packets
+uint8_t bufferIndex = 0; // Index to keep track of the current packet
+
 void setupPins(){
   pinMode(SLE_PIN, OUTPUT);
   pinMode(TRANSMIT_PIN, OUTPUT);
   pinMode(RECIEVE_PIN, INPUT);
   pinMode(CLK_OUT_PIN, INPUT);
   pinMode(SCLK_PIN, OUTPUT);
+
+  pinMode(FS_RX_PIN, INPUT);
+  pinMode(FS_TX_PIN, OUTPUT);
 
   digitalWrite(SLE_PIN, LOW); 
   digitalWrite(TRANSMIT_PIN, LOW); 
@@ -87,14 +98,66 @@ void transmitData(uint32_t data) {
   Serial.println("Data sent");
 }
 
+void receiveTelemetry() {
+  if (Serial1.available() > 0) {
+    uint8_t packet[MAX_PACKET_SIZE];
+    uint8_t length = 0;
+    
+    // Read telemetry packet from UART
+    while (Serial1.available() > 0 && length < MAX_PACKET_SIZE) {
+      packet[length++] = Serial1.read();
+    }
+
+    // Store packet in the buffer
+    if (bufferIndex < BUFFER_SIZE) {
+      memcpy(telemetryBuffer[bufferIndex], packet, length);
+      packetLengths[bufferIndex] = length;
+      bufferIndex++;
+    } else {
+      // Buffer is full, handle overflow
+      Serial.println("Buffer overflow");
+    }
+  }
+}
+
+void printTelemetry() {
+  for (uint8_t i = 0; i < bufferIndex; i++) {
+    Serial.print("Packet ");
+    Serial.print(i);
+    Serial.print(": ");
+    for (uint8_t j = 0; j < packetLengths[i]; j++) {
+      Serial.print(telemetryBuffer[i][j], BIN);
+      Serial.print(" ");
+    }
+    Serial.println();
+  }
+  bufferIndex = 0; // clear buffer
+}
+
+void transmitTelemetryBuffer() {
+  for (uint8_t i = 0; i < bufferIndex; i++) {
+    Serial.print("Transmitting Packet ");
+    Serial.print(i);
+    Serial.println(":");
+    for (uint8_t j = 0; j < packetLengths[i]; j++) {
+      // Assuming each byte of data is sent in sequence.
+      transmitData(telemetryBuffer[i][j]);
+    }
+    Serial.println("Packet transmitted");
+  }
+  bufferIndex = 0; // Clear buffer after transmission
+}
+
+
 void setup() {
   setupPins();
-  Serial.begin(115200);
+  Serial.begin(9600);
+  Serial1.begin(9600); // FS Serial initialization
   setupTransmitter();  
 }
 
 void loop() {
   // transmit dummy data
-  transmitData(0xAA);
-  delay(100);
+  receiveTelemetry();
+  printTelemetry();
 }
